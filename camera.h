bf7d9d5dd8b7364f70df0478b94a2db29f9db84b
Archead <greymer@pm.me>
2023-03-31 14:04:41 -0700

    create Camera class

---
+++ camera.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+enum MovementDirection
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+class Camera
+{
+public:
+	float yaw, pitch, fov, sensitivity, moveSpeed;
+	glm::vec3 cameraPos, cameraFront, cameraUp;
+
+
+	Camera(glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f),
+		glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f),
+		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f),
+		float fov = 90.0f,
+		float pitch = 0.0f,
+		float yaw = -90.0f, // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+		float sensitivity = 0.1,
+		float moveSpeed = 2.5)
+	{
+		this->cameraPos = cameraPos;
+		this->cameraFront = cameraFront;
+		this->cameraUp = cameraUp;
+		this->pitch = pitch;
+		this->yaw = yaw;
+		this->fov = fov;
+		this->sensitivity = sensitivity;
+		this->moveSpeed = moveSpeed;
+	}
+
+	glm::mat4 getViewMatrix()
+	{
+		return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); // cameraPos + cameraFront ensure the camera keeps looking at the same spot while moving
+	}
+
+	void keyboardMovement(MovementDirection direction, float deltaTime)
+	{
+		float velocity = moveSpeed * deltaTime;
+		if (direction == FORWARD)
+			cameraPos += velocity * cameraFront;
+		if (direction == BACKWARD)
+			cameraPos -= velocity * cameraFront;
+		if (direction == LEFT)
+			cameraPos += velocity * glm::cross(cameraUp, cameraFront);
+		if (direction == RIGHT)
+			cameraPos -= velocity * glm::cross(cameraUp, cameraFront);
+		if (direction == UP)
+			cameraPos += velocity * cameraUp;
+		if (direction == DOWN)
+			cameraPos -= velocity * cameraUp;
+
+	}
+
+	void mouseMovement(float *lastX, float *lastY, double xposIn, double yposIn)
+	{
+		float xpos = (float)xposIn;
+		float ypos = (float)yposIn;
+
+		float xoffset = xpos - *lastX;
+		float yoffset = *lastY - ypos; // swap this around for inverted (flight stick) style controls
+		*lastX = xpos;
+		*lastY = ypos;
+
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		yaw += xoffset;
+		pitch += yoffset;
+
+		if (pitch > 89.0f) pitch = 89.0f;
+		if (pitch < -89.0f) pitch = -89.0f;
+
+		glm::vec3 front;
+		front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		front.y = sin(glm::radians(pitch));
+		front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraFront = glm::normalize(front);
+	}
+};
--- main.cpp
+++ main.cpp
@@ -15,15 +15,28 @@
 #include "vendor/imgui/imgui_impl_opengl3.h"
 #include "vendor/imgui/imgui_impl_glfw.h"
 
-void processInput(GLFWwindow* window);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#include "vertices.h"
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+#include "camera.h"
+
+void processInput(GLFWwindow* window); // for continous key press
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods); // single key presses, i.e toggles
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+
+int resWidth = 800;
+int resHeight = 600;
+
+float lastX = (float)resWidth / 2.0;
+float lastY = (float)resHeight / 2.0;
+bool firstMouse = true;
 
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
+
+bool mouseToggle = false;
+
+Camera camera;
 
 int main()
 {
@@ -31,7 +44,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(resWidth, resHeight, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -44,64 +57,22 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, resWidth, resHeight);
+
+    //---------------------CALLBACK SETS------------------------
+    glfwSetKeyCallback(window, key_callback);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    //----------------------------------------------------------
+
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     Shader shader("shader.vert", "shader.frag");
     shader.use();
 
-    float vertices[] = {
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-    unsigned int indices1[] = {  // note that we start from 0!
-        0, 1, 2,   
-        0, 2, 3   
-    };
-
     VertexArray va;
     VertexBuffer vb(vertices, sizeof(vertices));
-    ElementBuffer eb(indices1, sizeof(indices1));
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -117,7 +88,7 @@ int main()
     unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
     unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
 
-    //----------------------IMGUI-------------------------------------
+    //----------------------IMGUI INIT-------------------------------
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -138,18 +109,17 @@ int main()
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     //----------------------------------------------------------------
 
-
     float rotationdeg = 0.0f;
-    float modelX, modelY, modelZ;
-    modelY = 1.0f;
-    modelX = modelZ = 0.00f;
+    glm::vec3 modelAxis = glm::vec3(0.0f, 1.0f, 0.0f);
     float spinSpeed = 0.5f;
     bool spin = false;
+
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
-        glClearColor(1.0f, 1.0f, 0.7f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.use();
@@ -159,36 +129,43 @@ int main()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(rotationdeg), glm::vec3(modelX, modelY, modelZ));
+        model = glm::rotate(model, glm::radians(rotationdeg), glm::vec3(modelAxis.x, modelAxis.y, modelAxis.z));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); // cameraPos + cameraFront ensure the camera keeps looking at the same spot while moving
 
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glm::mat4 view = camera.getViewMatrix(); 
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        glm::mat4 projection = glm::perspective(glm::radians(camera.fov), (float)resWidth / float(resHeight), 0.1f, 100.0f);
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         va.bind();
         glDrawArrays(GL_TRIANGLES, 0, 36);
         va.unbind();
 
-        { // ImGui Menu Items
+        // ImGui Menu Items
+        {   
             ImGui::Begin("Debug Menu");                          // Create a window called "Debug Menu" and append into it.
-            ImGui::SliderFloat("Rotation Degree", &rotationdeg, 0.0f, 360.0f);
-            ImGui::Checkbox("Continuous Spin", &spin);      // Edit bools storing our window open/close state
+            ImGui::Text("Press M to toggle mouse");
+            ImGui::Text("Model Rotation Matrix:");
+            ImGui::Checkbox("Continuous Spin", &spin);           // Edit bools storing our window open/close state
             ImGui::SliderFloat("Spin Speed", &spinSpeed, 0.0f, 10.0f);
-            if (spin)
-                rotationdeg += spinSpeed;
-            ImGui::SliderFloat("X", &modelX, 0.01f, 1.0f);
-            ImGui::SliderFloat("Y", &modelY, 0.01f, 1.0f);
-            ImGui::SliderFloat("Z", &modelZ, 0.01f, 1.0f);
+            if (spin) rotationdeg += spinSpeed;
+            ImGui::SliderFloat3("XYZ", glm::value_ptr(modelAxis), 0.01f, 1.0f);
+            ImGui::Text("FOV:");
+            ImGui::SliderFloat("FOV Scale", &camera.fov, 1.0f, 120.0f);
+            ImGui::Text("Camera Yaw and Pitch Values:");
+            ImGui::Text("Yaw: %f", camera.yaw);
+            ImGui::Text("Pitch: %f", camera.pitch);
+            ImGui::Text("Camera Coords:");
+            ImGui::Text("X: %f", camera.cameraPos.x);
+            ImGui::Text("Y: %f", camera.cameraPos.y);
+            ImGui::Text("Z: %f", camera.cameraPos.z);
+
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
         }
@@ -208,7 +185,6 @@ int main()
 
     glDeleteVertexArrays(1, &va.ID);
     glDeleteBuffers(1, &vb.ID);
-    glDeleteBuffers(1, &eb.ID);
     glfwTerminate();
     return 0;
 }
@@ -218,18 +194,35 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    const float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
+        camera.keyboardMovement(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
+        camera.keyboardMovement(BACKWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.keyboardMovement(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.keyboardMovement(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.keyboardMovement(UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+        camera.keyboardMovement(DOWN, deltaTime);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    resWidth = width;
+    resHeight = height;
     glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    camera.mouseMovement(&lastX, &lastY, xposIn, yposIn);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_M && action == GLFW_PRESS) mouseToggle = !mouseToggle;
+    if (mouseToggle) glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    else glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
